b80fb422cff5f475b5a286a8b6f65cb45d2bb1c1
eduardo329 <eduardo_rj_18@hotmail.com>
2021-10-08 16:41:10 -0300

    Projeto finalizado

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,21 +1,38 @@
 import { Photo } from '../types/Photo';
 import { storage } from '../libs/firebase';
-import { ref, listAll, getDownloadURL } from 'firebase/storage';
+import { ref, listAll, getDownloadURL, uploadBytes } from 'firebase/storage';
+import { v4 as createId } from 'uuid';
 
-export const  getAll = async () => {
+export const getAll = async () => {
   let list: Photo[] = [];
 
-const imagesFolder  = ref(storage, "images");
-const photoList = await listAll(imagesFolder);
+  const imagesFolder = ref(storage, "images");
+  const photoList = await listAll(imagesFolder);
 
-  for(let i in photoList.items) {
+  for (let i in photoList.items) {
     let photoURL = await getDownloadURL(photoList.items[i]);
 
     list.push({
       name: photoList.items[i].name,
       url: photoURL
     });
-  } 
+  }
 
   return list;
+}
+
+// Verificando se o arquivo é uma imagem
+export const insert = async (file: File) => {
+  if (['image/jpeg', 'image/jpg', 'image/png'].includes(file.type)) {
+
+    let randomName = createId();
+    let newFile = ref(storage, `images/${randomName}`)
+
+    let upload = await uploadBytes(newFile, file);
+    let photoUrl = await getDownloadURL(upload.ref);
+
+    return { name: upload.ref.name, url: photoUrl } as Photo;
+  } else {
+    return new Error('Tipo de arquivo não permitido.')
+  }
 }
